4c6ea1a5f849abf2c3eaa2fb50c7a107fc584489
raftouch <rafanelkin@gmail.com>
2025-04-23 14:50:50 +0200

    templates

--- playground/views.py
+++ playground/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse('Hello')
+    return render(request, 'hello.html', {
+        'page_name': 'Dashboard'
+    })
+    # return HttpResponse('Hello')
